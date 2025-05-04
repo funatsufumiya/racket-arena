7c5aed119407610cf3ae306e59e0d6113ce7b065
Fumiya Funatsu <funatsu.fumiya@gmail.com>
2025-05-04 13:55:52 +0900

    chore: update info

--- info.rkt
+++ info.rkt
@@ -5,3 +5,5 @@
 (define build-deps '("scribble-lib" "racket-doc"))
 (define pkg-desc "Arena allocation (memory segment) for Racket")
 (define version "0.1")
+(define license 'WTFPL)
+(define author "Fumiya Funatsu")
